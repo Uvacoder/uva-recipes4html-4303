646a1162ec225915535d2fa1cc6c956a826a3986
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-26 01:38:36 -0500

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: process.env.URL || "http://localhost:8080",
+  url: process.env.URL || "https://htmlrecipes-2.vercel.app",
   siteName: "HTML Recipes",
   siteDescription:
     "A collection of quick copy HTML snippets for a variety of common scenarios, created by Stephanie Eckles (@5t3ph).",
